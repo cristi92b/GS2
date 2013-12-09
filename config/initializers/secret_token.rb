9a72332cac5fd097c8061662a4afd758863f4003
Cristi B <cristi92b@gmail.com>
2013-12-09 19:18:15 +0200

    Commit #4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GS::Application.config.secret_token = 'a0beca31dc8bff7626fc7ca6ac8b6a7a7b0d0d6c9b52e9cab9942e8e05da2d8da26b1464143571d4380a67f659f97f1adc6a3373ba3c4f6ebfe78ffeb4ca476b'
